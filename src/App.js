dae80e1b5ba4bc3f2009de7ef1a28644d41281c4
rickyjw <rickywid@hotmail.com>
2019-06-24 21:51:20 -0400

    add circleci config

--- src/App.js
+++ src/App.js
@@ -1,44 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import styled from 'styled-components';
+import './App.css';
 
 const Button = styled.button`
   border: 5px solid red;
 `;
 
 class App extends React.Component {
-
   componentDidMount() {
-
-    const url = 'https://bookstax-api.herokuapp.com/';
+    // const url = 'https://bookstax-api.herokuapp.com/';
     const local = 'http://localhost:3001';
 
-    fetch(local).then(res=>{
-      return res.json();
-    }).then(json=>{
-      console.log(JSON.stringify(json));
-    })
+    fetch(local)
+      .then(res => res.json())
+      .then(json => console.log(JSON.stringify(json)));
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <Button>Click</Button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Button>styled-component button</Button>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+// import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// Pro Tip: You should write tests from a users point of view
+
+// Avoid .simulate() https://github.com/airbnb/enzyme/issues/1606
+
+// Jest Snapshots: Tests for any UI changes
+// https://jestjs.io/docs/en/snapshot-testing
+
+// Enzyme API
+// https://airbnb.io/enzyme/
+
+// Jest API
+//
+
+describe('Button exists', () => {
+  it('check to see if button exists on page', () => {
+    const wrapper = shallow(<App />);
+
+    const link = wrapper.find('a').text();
+    expect(link).toEqual('Learn React'); // PASS
+  });
 });
