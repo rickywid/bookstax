099f3d2bac50256d79f5c66d31e4ea1cc1741f50
rickyjw <rickywid@hotmail.com>
2019-07-01 19:00:09 -0400

    add search functionality and add books to backlog list

--- src/components/landing.js
+++ src/components/landing.js
@@ -2,14 +2,60 @@ import React from 'react';
 // import styled from 'styled-components';
 
 class Landing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+    };
+
+    this.saveBook = this.saveBook.bind(this);
+  }
+
   componentDidMount() {
     console.log('landing mounted');
   }
 
+  saveBook = (book) => {
+    const data = {
+      content: {
+        title: book.title,
+        author: book.authors[0],
+        cover: book.imageLinks.smallThumbnail,
+        isbn: book.industryIdentifiers[0].identifier,
+        status: 'backlog',
+      },
+    };
+
+    // save book to user's backlog
+    fetch('http://localhost:3001/user/addbook/1', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    }).then(res => console.log(res));
+  }
+
+  handleSearch() {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=malcolm+gladwell?printType=books&maxResults=40').then(res => res.json()).then((json) => {
+      this.setState({ results: [json] });
+    });
+  }
+
   render() {
+    const { results } = this.state;
     return (
       <div className="landing">
-        <p>Landing page</p>
+        <input type="text" placeholder="search title, author, isbn" />
+        <button onClick={this.handleSearch.bind(this)} type="button">Search</button>
+        {results.length > 0 ? results[0].items.map(book => (
+          <div>
+            <p>{book.volumeInfo.title}</p>
+            <button type="button" onClick={() => this.saveBook(book.volumeInfo)}>save</button>
+          </div>
+        ))
+          : ''
+        }
       </div>
     );
   }
