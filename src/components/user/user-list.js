8aa3de82b58f2d41f03e8aaf85516d159c89372c
rickyjw <rickywid@hotmail.com>
2019-07-05 14:50:11 -0400

    remove console log

--- src/components/user/user-list.js
+++ src/components/user/user-list.js
@@ -38,7 +38,7 @@ class UserList extends React.Component {
       columnOrder: ['backlog', 'completed', 'current'],
     };
 
-    Promise.all([fetch('http://localhost:3001/user/list/likes'), fetch('http://localhost:3001/user/1')]).then((res) => {
+    Promise.all([fetch('http://localhost:3001/user/list/likes'), fetch('http://localhost:3001/user/bookshelf/1')]).then((res) => {
       Promise.all([res[0].json(), res[1].json()]).then((res2) => {
         const isLiked = res2[0];
         const books = [...res2[1][0].backlog, ...res2[1][0].currently, ...res2[1][0].completed];
