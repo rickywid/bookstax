059fcaa5396a2bca5afc470a2dd645499be552ad
rickyjw <rickywid@hotmail.com>
2019-08-10 04:49:49 -0400

    add antdesign inputs for search and login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { compose } from 'redux';
-import { Field, reduxForm } from 'redux-form';
 import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import { Button } from 'antd';
+import { Button, Input } from 'antd';
 import Routes from './routes';
 import * as actions from './actions/simpleAction';
 import Logo from './assets/images/logo.png';
@@ -33,27 +31,13 @@ const NavItems = styled.li`
   padding-right: 1rem;
 `;
 
-const InputWrapper = styled.div`
-  display: inline-block;
-`;
-
-const Form = styled.form`
-  input {
-    background: none;
-    border: 1px solid grey;
-    border-radius: 4px;
-    padding: 0 5px;
-    height: 32px;
-    margin-right: 1rem;
-  }
-  display: flex;
-  margin-right: 1rem;
-`;
-
 const Container = styled.div`
   max-width: 1140px;
   margin: 0 auto;
 `;
+
+const { Search } = Input;
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -77,26 +61,20 @@ class App extends React.Component {
     this.setState({ user: nextProps.user });
   }
 
-  onFormSubmit(values) {
-    const { searchResults, history, reset } = this.props;
-    searchResults(values, history);
-    reset();
+  onFormSubmit(e = {}, values = {}) {
+    let query = values;
+    if (e) {
+      query = e.target.value;
+    }
+
+    const { searchResults, history } = this.props;
+    searchResults(query, history);
   }
 
   signout() {
     const { signOut } = this.props;
     signOut();
   }
-
-  renderField = ({
-    input, label, type, meta: { touched, error },
-  }) => (
-    <InputWrapper>
-      <input {...input} placeholder={label} type={type} />
-      {touched
-        && ((error && <p className='error'>{error}</p>))}
-    </InputWrapper>
-  )
 
   renderNavLinks() {
     const { isAuth } = this.props;
@@ -138,17 +116,18 @@ Bookshelf
   }
 
   render() {
-    const { handleSubmit } = this.props;
-
     return (
       <div className='App'>
         <NavBar>
           <Link to='/'><img style={{ width: '65%' }} src={Logo} alt='logo' /></Link>
+          <Search
+            placeholder='search book...'
+            onSearch={values => this.onFormSubmit(null, values)}
+            onPressEnter={e => this.onFormSubmit(e)}
+            style={{ width: 300 }}
+            allowClear
+          />
           <NavRightSide>
-            <Form onSubmit={handleSubmit(this.onFormSubmit.bind(this))}>
-              <Field component={this.renderField} type='text' name='search' label='search title, author, isbn' />
-              <Button default htmlType='submit'>Search</Button>
-            </Form>
             <NavList>{this.renderNavLinks()}</NavList>
           </NavRightSide>
         </NavBar>
@@ -160,23 +139,12 @@ Bookshelf
   }
 }
 
-const validate = (values) => {
-  const errors = { values };
-  if (!values.search) {
-    errors.search = 'Cannot be blank';
-  }
-
-  return errors;
-};
-
 function mapStateToProps(state) {
   return {
     user: state.getUser,
     isAuth: state.auth,
   };
 }
-
-// export default connect(mapStateToProps, actions)(App);
 
 App.propTypes = {
   user: PropTypes.shape({}).isRequired,
@@ -185,20 +153,6 @@ App.propTypes = {
   getLoggedInUserProfile: PropTypes.func.isRequired,
   searchResults: PropTypes.func.isRequired,
   history: PropTypes.shape({}).isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  error: PropTypes.func,
-  reset: PropTypes.func.isRequired,
 };
 
-App.defaultProps = {
-  error: PropTypes.func,
-};
-
-export default compose(
-  reduxForm({
-    form: 'searchBooks',
-    fields: ['search'],
-    validate,
-  }),
-  connect(mapStateToProps, actions),
-)(withRouter(App));
+export default connect(mapStateToProps, actions)(withRouter(App));
