a37dc1ed2afde558f50dbaf4e55dbf9b533f944b
rickyjw <rickywid@hotmail.com>
2019-08-18 03:06:08 -0400

    style 404 page

--- src/components/noMatch.js
+++ src/components/noMatch.js
@@ -1,5 +1,22 @@
 import React from 'react';
-// import styled from 'styled-components';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { Icon } from 'antd';
+
+const Wrapper = styled.div`
+  height: calc(100vh - 193px);
+  display: flex;
+  margin: auto;
+`;
+const InnerWrapper = styled.div`
+  margin: auto;
+`;
+const IconStyle = styled(Icon)`
+  display: block;
+  font-size: 28px;
+  display: block !important;
+  margin-bottom: 2rem;
+`;
 
 class NoMatch extends React.Component {
   componentDidMount() {
@@ -8,9 +25,16 @@ class NoMatch extends React.Component {
 
   render() {
     return (
-      <div className="no-match">
-        <p>No Match</p>
-      </div>
+      <Wrapper className="animated fadeIn">
+        <InnerWrapper>
+          <IconStyle type="home" />
+          <h1>The page you are looking for does not exist</h1>
+          <p style={{ textAlign: 'center' }}>
+            Go back to&nbsp;
+            <Link to="/home">homepage</Link>
+          </p>
+        </InnerWrapper>
+      </Wrapper>
     );
   }
 }
